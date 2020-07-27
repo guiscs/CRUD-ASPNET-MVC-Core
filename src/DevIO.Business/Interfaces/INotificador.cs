7db980f3400c6916b9790251435c574cdf9e76f9
Guilherme Mello <gui.scs@gmail.com>
2020-07-26 22:33:57 -0300

    Subindo todo o fonte do projeto

---
+++ src/DevIO.Business/Interfaces/INotificador.cs
@@ -0,0 +1,12 @@
+﻿using DevIO.Business.Notificacoes;
+using System.Collections.Generic;
+
+namespace DevIO.Business.Interfaces
+{
+    public interface INotificador
+    {
+        bool TemNotificacao();
+        List<Notificacao> ObterNotificacao();
+        void Handle(Notificacao notificacao);
+    }
+}
---
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,40 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace DevIO.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Seta um padrao de criação de tipo caso ele nao seja mapeado
+            // Ex: uma string, seria cadastrada como nVarchar(max), mas desta fora será criada como varchar(100)
+            //foreach (var property in modelBuilder.Model.GetEntityTypes()
+            //    .SelectMany(e => e.GetProperties()
+            //    .Where(p => p.ClrType == typeof(string))))
+            //    property.Relational().ColumnType = "varchar(100)";
+
+
+
+            //Aqui faz com que na criação pegue tadoas a entidades mapeadas relacionadas ao Dbcontext
+            // Vai registrar todos os mappings
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            // Desabilitar o Cascade Delete
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
